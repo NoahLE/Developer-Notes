594a55debfd9b7926592ee64c6d250698b85525b
NoahLE <noah@noahle.com>
2018-02-27 22:18:29 -0800

    done with the tutorial

--- gatsby-src/gatsby-config.js
+++ gatsby-src/gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-glamor`,
+        `gatsby-transformer-remark`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
